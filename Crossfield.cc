f417e86f3c421da5cf99cea0a74aaae2d0abd94f
wuschelbueb <Wuschelbueb@users.noreply.github.com>
2021-09-28 14:56:41 +0200

    polished getBaryCenterAndRefEdge
    removed colored faces

--- Crossfield.cc
+++ Crossfield.cc
@@ -18,16 +18,13 @@ void Crossfield::setlocalCoordFrame() {
 }
 
 void Crossfield::getBaryCenterAndRefEdge(std::vector<Point> &barycenters) {
+    // status needs to be released before using, in case it still has saved some stuff from before
     trimesh_.release_halfedge_status();
     trimesh_.release_face_status();
+    // request to change the status
     trimesh_.request_halfedge_status();
     trimesh_.request_face_status();
     std::vector<int> constraints;
-    std::vector<int> faces;
-    TriMesh::Color white = {1, 1, 1, 1};
-    TriMesh::Color green = {0, 1, 0, 1};
-
-    int counter = 0;
     getConstraints(constraints);
 
     // assign constraint edges to their faces
@@ -39,18 +36,19 @@ void Crossfield::getBaryCenterAndRefEdge(std::vector<Point> &barycenters) {
         OpenMesh::HalfedgeHandle heh = trimesh_.halfedge_handle(eh, 1);
         if (trimesh_.is_boundary(heh))
             heh = trimesh_.opposite_halfedge_handle(heh);
-
         OpenMesh::FaceHandle fh = trimesh_.face_handle(heh);
         for (auto fv_it = trimesh_.fv_iter(fh); fv_it.is_valid(); ++fv_it) {
             bCenter += trimesh_.point(*fv_it);
             ++valence;
         }
         barycenters.push_back(bCenter / valence);
+        // add barycenter to face
         trimesh_.property(barycenter, fh) = (bCenter / valence);
+        // add reference edge to face
         trimesh_.property(reference_edge, fh) = trimesh_.calc_edge_vector(heh);
+        // set face and edge as used
         trimesh_.status(heh).set_tagged(true);
         trimesh_.status(fh).set_tagged(true);
-        faces.push_back(fh.idx());
     }
 
     // assign reference edges in range to faces
@@ -62,9 +60,10 @@ void Crossfield::getBaryCenterAndRefEdge(std::vector<Point> &barycenters) {
             heh = trimesh_.opposite_halfedge_handle(heh);
         OpenMesh::FaceHandle fh = trimesh_.face_handle(heh);
         OpenMesh::HalfedgeHandle nheh = trimesh_.next_halfedge_handle(heh);
+        // halfedges or faces which are already tagged, won't be used anymore, no duplicates are allowed
         if ((std::find(heInRange_.begin(), heInRange_.end(), nheh.idx()) != heInRange_.end()) &&
             !trimesh_.status(heh).tagged() && !trimesh_.status(fh).tagged()) {
-            ++counter;
+
             for (auto fv_it = trimesh_.fv_iter(fh); fv_it.is_valid(); ++fv_it) {
                 bCenter += trimesh_.point(*fv_it);
                 ++valence;
@@ -74,26 +73,8 @@ void Crossfield::getBaryCenterAndRefEdge(std::vector<Point> &barycenters) {
             trimesh_.property(reference_edge, fh) = trimesh_.calc_edge_vector(heh);
             trimesh_.status(heh).set_tagged(true);
             trimesh_.status(fh).set_tagged(true);
-            faces.push_back(fh.idx());
         }
     }
-
-    for (OpenMesh::FaceHandle fh: trimesh_.faces()) {
-        trimesh_.property(face_color, fh);
-        trimesh_.set_color(fh, white);
-    }
-
-    for (int i: faces) {
-        OpenMesh::FaceHandle fh = trimesh_.face_handle(i);
-        trimesh_.property(face_color, fh);
-        trimesh_.set_color(fh, green);
-    }
-
-    std::cout << "he in range: " << heInRange_.size()
-              << "\nconstraints: " << constraints.size()
-              << "\nand we counted: " << counter
-              << "\nwhich gives us the sum: " << counter + constraints.size()
-              << "\nwith faces: " << faces.size() << std::endl;
 }
 
 // convert array of halfedges to faces and fill constraints with them
@@ -110,24 +91,30 @@ void Crossfield::getConstraints(std::vector<int> &constraints) {
             for (int j: selectedVertices) {
                 OpenMesh::VertexHandle vhj = trimesh_.vertex_handle(j);
                 OpenMesh::HalfedgeHandle heh = trimesh_.find_halfedge(vhj, vh);
+                // avoids duplicates with std::find
                 if (heh.is_valid() &&
                     !(std::find(constraints.begin(), constraints.end(), trimesh_.edge_handle(heh).idx()) !=
                       constraints.end())) {
                     constraints.push_back(trimesh_.edge_handle(heh).idx());
                 }
             }
-
         }
     }
 
-    if (!selectedEdges.empty())
-        for (int i: selectedEdges)
-            constraints.push_back(i);
+    if (!selectedEdges.empty()) {
+        for (int i: selectedEdges) {
+            // avoids duplicates with std::find
+            if (!(std::find(constraints.begin(), constraints.end(), i) != constraints.end()))
+                constraints.push_back(i);
+        }
+    }
 
     if (!selectedHEdges.empty()) {
         for (int i: selectedHEdges) {
             OpenMesh::EdgeHandle eh = trimesh_.edge_handle(trimesh_.halfedge_handle(i));
-            constraints.push_back(eh.idx());
+            // avoids duplicates with std::find
+            if (!(std::find(constraints.begin(), constraints.end(), eh.idx()) != constraints.end()))
+                constraints.push_back(eh.idx());
         }
     }
 
@@ -135,6 +122,7 @@ void Crossfield::getConstraints(std::vector<int> &constraints) {
         for (int i: selectedFaces) {
             OpenMesh::FaceHandle fh = trimesh_.face_handle(i);
             for (auto fe_it = trimesh_.fe_iter(fh); fe_it.is_valid(); ++fe_it) {
+                // avoids duplicates with std::find
                 if (!(std::find(constraints.begin(), constraints.end(), fe_it->idx()) != constraints.end()))
                     constraints.push_back(fe_it->idx());
             }
